be218ccdf8d69b913f0c3633a7eb8424ede5213e
W3Tools <ritter@w3tools.app>
2023-03-10 17:55:29 +0800

    update pssword auth

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,5 @@
 import { JWTService } from './jwt.service';
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { LoginReqDto, LoginRspDto } from '../dto/auth.dto';
 
 export const SESSION_PERFIX = 'login_session';
@@ -9,6 +9,11 @@ export class AuthService {
     constructor(private readonly jwtService: JWTService) {}
 
     async login(args: LoginReqDto): Promise<LoginRspDto> {
+        let username = process.env.USERNAME;
+        let password = process.env.PASSWORD;
+        if (!username || !password) throw Error('username or password not set');
+        if (username != args.username || password != args.password) throw new HttpException('password verification failure', HttpStatus.BAD_REQUEST);
+
         const accessToken = this.jwtService.createToken(args.username, args.password);
 
         return { session: accessToken };
